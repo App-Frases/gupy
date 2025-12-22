b8de067aa5f28c02dc13bcc8c0d70ddac0169bd8
pedrogabriels86 <pedrogabriels@gmail.com>
2025-12-22 13:13:37 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Configurações do Supabase
 const SUPABASE_URL = 'https://urmwvabkikftsefztadb.supabase.co';
 // SUBSTITUA PELA SUA CHAVE 'ANON' 'PUBLIC' REAL DO PAINEL DO SUPABASE
-const SUPABASE_KEY = 'SUA_NOVA_CHAVE_ANON_AQUI'; 
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVybXd2YWJraWtmdHNlZnp0YWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNjU1NjQsImV4cCI6MjA4MDc0MTU2NH0.SXR6EG3fIE4Ya5ncUec9U2as1B7iykWZhZWN1V5b--E'; 
 
 const _supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
